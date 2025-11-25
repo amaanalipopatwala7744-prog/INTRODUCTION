378d14e2d24625fe5bc49d379a62618cef69ee2e
amaanalipopatwala7744-prog <amaanalipopatwala7744@gmail.com>
2025-11-25 10:30:00 +0530

    Add files via upload

---
+++ idex.js
@@ -0,0 +1,7 @@
+// External file: idex.js
+document.addEventListener("DOMContentLoaded", function () {
+	var el = document.getElementById("demo");
+	if (el) {
+		el.innerHTML = "Paragraph changed.";
+	}
+});
